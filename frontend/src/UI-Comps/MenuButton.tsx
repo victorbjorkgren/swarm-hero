5e6ab594823100233a0ad6510c0827105df97a44
victorbjorkgren <victor.bjorkgren@piktiv.se>
2025-01-06 16:07:44 +0100

    Menu Button nudge

--- frontend/src/UI-Comps/MenuButton.tsx
+++ frontend/src/UI-Comps/MenuButton.tsx
@@ -13,7 +13,7 @@ export const MenuButton = ({ label, disabled, small, onClick }: Props) => {
             disabled={disabled}
             className={`bg-white flex items-center justify-center text-white 
                 bg-opacity-10 border border-white border-opacity-50 rounded-xl
-                ${small ? "font-mono text-lg p-5 w-32 h-9 tracking-widest" : "font-mono tracking-widest text-3xl p-9 w-64 h-12"}
+                ${small ? "font-mono text-lg p-5 w-32 h-9 tracking-widest" : "font-mono tracking-widest text-2xl p-9 w-64 h-12"}
                 ${!disabled ? "hover:bg-opacity-40 active:bg-opacity-90" : "opacity-50"}`}
             onClick={onClick}
         >
